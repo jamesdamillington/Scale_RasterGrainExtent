2f4f0bc95093cfa0f31ac5bddb6e8a6d9f50cf72
James Millington <jamesdamillington@gmail.com>
2020-05-05 20:41:28 +0100

    adding raster degrade and deextent functions

--- RasterGrainExtent.R
+++ RasterGrainExtent.R
@@ -3,13 +3,13 @@
 ###### Load packages
 library(raster)
 library(rasterVis)
-library(viridis)
 library(ggplot2)
 
 
 ######
 ###### Functions
 
+##create palette for plots
 lc_pal_function <- function(map){
   
   pal <- c()
@@ -26,6 +26,7 @@ lc_pal_function <- function(map){
   return(pal)
 }
 
+##create labels for plots
 lc_label_function <- function(map){
   
   pal <- c()
@@ -42,21 +43,9 @@ lc_label_function <- function(map){
   return(pal)
 }
 
-
-extent_TR <- function(ras){
-  
-  e <- extent(
-    xmin(ras)+((xmax(ras)-xmin(ras))/2), 
-    xmax(ras), 
-    ymin(ras)+((ymax(ras)-ymin(ras))/2), 
-    ymax(ras)
-  )
-  
-  return(e)
-}
-
+##plot raster using palette and labels 
 raster_plot <- function(ras){
- 
+  
   ras_plot <- rasterVis::gplot(ras) + 
     geom_tile(aes(fill = as.factor(value))) +
     scale_fill_manual(values = lc_pal_function(ras), labels=lc_label_function(ras)) +
@@ -66,6 +55,25 @@ raster_plot <- function(ras){
 }
 
 
+##crop raster to specified extent (top right)
+raster_deextent <- function(ras, f){
+  
+  #extent object for top right of input ras, scaled by f
+  e <- extent(
+    xmin(ras)+((xmax(ras)-xmin(ras))/f), 
+    xmax(ras), 
+    ymin(ras)+((ymax(ras)-ymin(ras))/f), 
+    ymax(ras)
+  )
+  
+  #crop, trim output
+  output <- raster::crop(ras, e)
+  output <- raster::trim(output)
+  
+  return(output)
+}
+
+##summarise raster (for chapter figures)
 raster_summary <- function(ras){
   
   rxres <- xres(ras)
@@ -74,15 +82,20 @@ raster_summary <- function(ras){
   print(paste0("Grain (m): ",rxres))  
   print(paste0("Extent (sq km): ",(rxres*rxres*rncell)/1000000))
   print(paste0("Cells (count): ",rncell))
-  print(paste0("Land Covers (count): ",length(unique(LandCov)))) #number of land covers
+  print(paste0("Land Covers (count): ",length(unique(ras)))) #number of land covers
   
 }
 
-#create plotting function
-#
-#create scaling function (to produce two maps from original)
-#
-#create similar extent function
+##degrade resolution of a raster by given factor (f)
+raster_degrade <- function(ras, f){
+  
+  output <- aggregate(ras, fact=f, fun=modal, expand=TRUE)
+  output <- focal(output,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
+  output <-raster::trim(output)
+  return(output)
+  
+}
+
 
 ######
 ###### Load data
@@ -93,11 +106,8 @@ raster_summary <- function(ras){
 #library(archive)
 #archive_extract(archive("covtyp_rc.7z"),dir=".","covtyp_rc.asc")
 
-#first load a raster map in which the value of each pixel is the distance to the nearest lowland conifer pixel
-LandCov <- raster("covtyp_rc.asc")             #simply provide file name
-crs(LandCov) = "+init=epsg:3160" 
-
-
+#original data (10m grain, 20 sq km extent)
+LandCov <- raster("covtyp_rc.asc")
 raster_summary(LandCov)
 
 LC_plot <- raster_plot(LandCov)
@@ -107,170 +117,93 @@ print(LC_plot)
 #ggsave("LC.pdf", LC_plot) 
 
 
+######
+###### Varying Grain
+
 ##### 20-40
-LandCov20 <- aggregate(LandCov, fact=2, fun=modal, expand=TRUE)
-LandCov20 <- focal(LandCov20,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
+LandCov20 <- raster_degrade(LandCov, f=2)
+LandCov40 <- raster_degrade(LandCov, f=4)
 
-area(LandCov20)
+raster_summary(LandCov20)
+raster_summary(LandCov40)
 
+LC_plot20 <- raster_plot(LandCov20)
+LC_plot40 <- raster_plot(LandCov40)
 
-LandCov40 <- aggregate(LandCov20, fact=2, fun=modal, expand=TRUE)
-LandCov40 <- focal(LandCov40,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov40 <- raster::trim(LandCov40)
+print(LC_plot20)
+print(LC_plot40)
 
-LC40_plot <- gplot(LandCov40) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov40), labels=lc_label_function(LandCov40)) +
-  labs(fill="Land Cover", x="", y="")
-
-print(LC40_plot)
-ggsave("LC40.png", LC40_plot) 
-ggsave("LC40.pdf", LC40_plot)  
-ncell(LandCov40)
-
-##### 50-250
-LandCov50 <- aggregate(LandCov, fact=5, fun=modal, expand=TRUE)
-LandCov50 <- focal(LandCov50,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov50 <- raster::trim(LandCov50)
-
-area(LandCov50)
-LC_plot50 <- gplot(LandCov50) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
-
-print(LC_plot50)
-
-LandCov250 <- aggregate(LandCov50, fact=5, fun=modal, expand=TRUE)
-LandCov250 <- focal(LandCov250,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-
-area(LandCov250)
-LC_plot250 <- gplot(LandCov250) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
-
-print(LC_plot250)
+#ggsave("LC40.png", LC_plot40) 
+#ggsave("LC40.pdf", LC_plot40)  
 
 
-##### 40-160
-LandCov40a <- aggregate(LandCov, fact=4, fun=modal, expand=TRUE)
-LandCov40a <- focal(LandCov40a,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov40a <- raster::trim(LandCov40a)
+##### 30-90 (use this in final version?)
+LandCov30 <- raster_degrade(LandCov, f=3)
+LandCov90 <- raster_degrade(LandCov, f=9)
 
-area(LandCov40a)
-LC_plot40a <- gplot(LandCov40a) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
+raster_summary(LandCov30)
+raster_summary(LandCov90)
 
-print(LC_plot40a)
-
-LandCov160 <- aggregate(LandCov40a, fact=4, fun=modal, expand=TRUE)
-LandCov160 <- focal(LandCov160,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov160 <- raster::trim(LandCov160)
-
-area(LandCov160)
-LC_plot160 <- gplot(LandCov160) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
-
-print(LC_plot160)
-
-
-
-##### 30-90
-LandCov30 <- aggregate(LandCov, fact=3, fun=modal, expand=TRUE)
-LandCov30 <- focal(LandCov30,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov30 <- raster::trim(LandCov30)
-
-area(LandCov30)
-LC_plot30 <- gplot(LandCov30) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
+LC_plot30 <- raster_plot(LandCov30)
+LC_plot90 <- raster_plot(LandCov90)
 
 print(LC_plot30)
-
-LandCov90 <- aggregate(LandCov30, fact=3, fun=modal, expand=TRUE)
-LandCov90 <- focal(LandCov90,  w = matrix(1, nrow = 3, ncol = 3), fun = modal)
-LandCov90 <- raster::trim(LandCov90)
-
-area(LandCov90)
-LC_plot90 <- gplot(LandCov90) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov), labels=lc_label_function(LandCov)) +
-  labs(fill="Land Cover", x="", y="")
-
 print(LC_plot90)
 
-
-# LandCov40 <- myRatify(LandCov40)
-# png("LC40.png", width=3600, height=3600, res=300)
-# levelplot(LandCov40, att = 'landcover', col.regions=lc_pal_function(LandCov40))  #something weired with order of the colours vs levels
-# dev.off()
-
-LC20_ext <- extent_TR(LandCov20) 
-LandCov20_sub <- raster::crop(LandCov20, LC20_ext)
-LandCov20_sub <- raster::trim(LandCov20_sub)
-
-LC20_plot <- gplot(LandCov20_sub) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov20_sub), labels=lc_label_function(LandCov20_sub)) +
-  labs(fill="Land Cover", x="", y="")
-
-print(LC20_plot)
-ggsave("LC20.png", LC20_plot)  
-ggsave("LC20.pdf", LC20_plot) 
-ncell(LandCov20_sub)
-
-# LandCov20_sub <- myRatify(LandCov20_sub)
-# png("LC20.png", width=3600, height=3600, res=300)
-# levelplot(LandCov20_sub, att = 'landcover', col.regions=lc_pal_function(LandCov20_sub))  #something weired with order of the colours vs levels
-# dev.off()
-
-LC10_ext <- extent_TR(LandCov20_sub) 
-LandCov10_sub <- raster::crop(LandCov, LC10_ext)
-LandCov10_sub <- raster::trim(LandCov10_sub)
-# LandCov_sub <- myRatify(LandCov_sub)
-# png("LC10.png", width=3600, height=3600, res=300)
-# levelplot(LandCov_sub, att = 'landcover', col.regions=lc_pal_function(LandCov_sub))  #something weired with order of the colours vs levels
-# dev.off()
+#ggsave("LC90.png", LC_plot90) 
+#ggsave("LC90.pdf", LC_plot90)  
 
 
-LC10_plot <- gplot(LandCov10_sub) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LandCov10_sub), labels=lc_label_function(LandCov10_sub)) +
-  labs(fill="Land Cover", x="", y="")
+######
+###### Varying Extent
 
-print(LC10_plot)
-ncell(LandCov10_sub)
+##40-20-10
+raster_summary(LandCov40)
 
-ggsave("LC10.png", LC10_plot)  
-ggsave("LC10.pdf", LC10_plot)
+LC20_quart <- raster_deextent(LandCov20, 2)
+raster_summary(LC20_quart)
+
+#for plotting extend to full extent of original data
+LC20_quart_ext <- extend(LC20_quart, LandCov)
+LC20_quart_plot <- raster_plot(LC20_quart_ext)
+print(LC20_quart_plot)
+
+#ggsave("LC20_quart_ext.png", LC20_quart_plot)  
+#ggsave("LC20_quart_ext.pdf", LC20_quart_plot) 
+
+LC10_quart <- raster_deextent(LandCov, 1.333)
+raster_summary(LC10_quart)
+
+#for plotting extend to full extent of original data
+LC10_quart_ext <- extend(LC10_quart, LandCov)
+LC10_quart_plot <- raster_plot(LC10_quart_ext)
+print(LC10_quart_plot)
+
+#ggsave("LC10_quart_ext.png", LC10_quart_plot)  
+#ggsave("LC10_quart_ext.pdf", LC10_quart_plot) 
 
 
-LC10_extend <- extend(LandCov10_sub, LandCov)
-LC20_extend <- extend(LandCov20_sub, LandCov)
+##90-30-10 (use this in final version?)
+raster_summary(LandCov90)
 
+LC30_third <- raster_deextent(LandCov30, 1.47)
+raster_summary(LC30_third)
 
-LC10_extend_plot <- gplot(LC10_extend) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LC10_extend), labels=lc_label_function(LC10_extend)) +
-  labs(fill="Land Cover", x="", y="")
+#for plotting extend to full extent of original data
+LC30_third_ext <- extend(LC30_third, LandCov)
+LC30_third_plot <- raster_plot(LC30_third_ext)
+print(LC30_third_plot)
 
-plot(LC10_extend_plot)
-ggsave("LC10_extend.png", LC10_extend_plot)  
-ggsave("LC10_extend.pdf", LC10_extend_plot)
+#ggsave("LC20_quart_ext.png", LC20_quart_plot)  
+#ggsave("LC20_quart_ext.pdf", LC20_quart_plot) 
 
-LC20_extend_plot <- gplot(LC20_extend) + 
-  geom_tile(aes(fill = as.factor(value))) +
-  scale_fill_manual(values = lc_pal_function(LC20_extend), labels=lc_label_function(LC20_extend)) +
-  labs(fill="Land Cover", x="", y="")
+LC10_ninth <- raster_deextent(LandCov, 1.12)
+raster_summary(LC10_ninth)
 
-plot(LC20_extend_plot)
-ggsave("LC20_extend.png", LC20_extend_plot)  
-ggsave("LC20_extend.pdf", LC20_extend_plot)
+#for plotting extend to full extent of original data
+LC10_ninth_ext <- extend(LC10_ninth, LandCov)
+LC10_ninth_plot <- raster_plot(LC10_ninth_ext)
+print(LC10_ninth_plot)
 
-plot(LandCov40)
+#ggsave("LC10_quart_ext.png", LC10_quart_plot)  
+#ggsave("LC10_quart_ext.pdf", LC10_quart_plot) 
